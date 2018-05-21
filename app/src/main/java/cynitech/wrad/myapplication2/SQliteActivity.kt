91c5d14e61310da515504378e7beb4c5a97986a4
Darwin Guzmán <dga_guz@hotmail.es>
2018-05-21 07:45:03 -0500

    SQlite terminado

--- app/src/main/java/cynitech/wrad/myapplication2/SQliteActivity.kt
+++ app/src/main/java/cynitech/wrad/myapplication2/SQliteActivity.kt
@@ -1,12 +1,18 @@
 package cynitech.wrad.myapplication2
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
 
 class SQliteActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sqlite)
+
+        val dbHandler = DbHandlerAplicacion(this)
+        dbHandler.insertarUsuario("Adrian")
+        dbHandler.leerDatos()
+        
     }
 }
