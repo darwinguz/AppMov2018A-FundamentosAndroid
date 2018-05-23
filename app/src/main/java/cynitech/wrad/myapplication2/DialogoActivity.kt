11db1dba569b3738982a7439c8fc6ecc5fcfa64d
Darwin Guzmán <dga_guz@hotmail.es>
2018-05-23 11:30:48 -0500

    Dialogo Multioptions arreglado

--- app/src/main/java/cynitech/wrad/myapplication2/DialogoActivity.kt
+++ app/src/main/java/cynitech/wrad/myapplication2/DialogoActivity.kt
@@ -18,7 +18,6 @@ class DialogoActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
-        }
 
 
 //        val builder = AlertDialog.Builder(this)
@@ -29,19 +28,19 @@ class DialogoActivity : AppCompatActivity() {
 //                .setNegativeButton("No", null)
 
 
-        val builder2 = AlertDialog.Builder(this)
-        val listaItems = resources.getStringArray(R.array.string_array_opciones_dialogo)
-        val seleccionUsuario = booleanArrayOf(true, false, false, false)
-        builder2.setTitle("Escoja un numero")
-                .setMultiChoiceItems(
-                        listaItems,
-                        seleccionUsuario,
-                        DialogInterface.OnMultiChoiceClickListener { dialogInterface: DialogInterface,
-                                                                     i: Int,
-                                                                     b ->
+            val builder2 = AlertDialog.Builder(this)
+            val listaItems = resources.getStringArray(R.array.string_array_opciones_dialogo)
+            val seleccionUsuario = booleanArrayOf(true, false, false, false)
+            builder2.setTitle("Escoja un numero")
+                    .setMultiChoiceItems(
+                            listaItems,
+                            seleccionUsuario,
+                            DialogInterface.OnMultiChoiceClickListener { dialogInterface: DialogInterface,
+                                                                         i: Int,
+                                                                         b ->
 
-                            Log.i("dialogo", "Escogio algo $i")
-                        })
+                                Log.i("dialogo", "Escogio algo $i")
+                            })
 //                .setSingleChoiceItems(listaItems/*arrayOf("1", "2", "3")*/, -1) { dialog, which ->
 //                    Log.i("dialogo","Selecciono $which")
 //                }
@@ -50,25 +49,26 @@ class DialogoActivity : AppCompatActivity() {
 //                }
 
 
-        builder2.setPositiveButton(
-                "Aceptar",
-                DialogInterface.OnClickListener { dialog, which ->
-                    Log.i("dialogo", "Acepto")
-                }
-        )
-        builder2.setNegativeButton(
-                "Cancelar",
-                null
-        )
+            builder2.setPositiveButton(
+                    "Aceptar",
+                    DialogInterface.OnClickListener { dialog, which ->
+                        Log.i("dialogo", "Acepto")
+                    }
+            )
+            builder2.setNegativeButton(
+                    "Cancelar",
+                    null
+            )
 
-        val dialogo2 = builder2.create()
-        dialogo2.show()
+            val dialogo2 = builder2.create()
+            dialogo2.show()
 
 
 //        val dialogo = builder.create()
 //        dialogo.show()
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+
+        }
+
     }
 
 }
